8ceaabb62feb9430fcc7d2fdb0a22f29067f89a9
Dan Erickson <druckson@gmail.com>
2013-12-19 13:24:58 -0800

    fix volume rendering shader

--- scene.go
+++ scene.go
@@ -2,7 +2,8 @@ package main
 
 import ("github.com/go-gl/gl"
         glfw "github.com/go-gl/glfw3"
-        "fmt")
+        "fmt"
+        "math")
 
 var _vr *VolumetricRenderer
 
@@ -16,10 +17,21 @@ func SetWindowSize(window *glfw.Window, width int, height int) {
 
 type VolumetricRenderer struct {
     window *glfw.Window
-    volumeData gl.Texture
     program gl.Program
+
+    volumeDataTexture gl.Texture
+
+    volumeData gl.UniformLocation
     width gl.UniformLocation
     height gl.UniformLocation
+    samples gl.UniformLocation
+
+    test gl.UniformLocation
+
+    angle gl.UniformLocation
+    near gl.UniformLocation
+    far gl.UniformLocation
+
     position gl.UniformLocation
     up gl.UniformLocation
     focus gl.UniformLocation
@@ -42,21 +54,16 @@ func CreateVolumetricRenderer() *VolumetricRenderer {
 
     vr.window.SetSizeCallback(SetWindowSize)
     vr.window.MakeContextCurrent()
-    glfw.SwapInterval(1)
     gl.Init()
-
 
     gl.ClearColor(0.5, 0.5, 0.5, 0)
 
-    gl.Viewport(0, 0, 100, 100)
+    //gl.Viewport(0, 0, 100, 100)
     gl.MatrixMode(gl.PROJECTION)
     gl.LoadIdentity()
     gl.MatrixMode(gl.MODELVIEW)
     gl.LoadIdentity()
 
-    dim := 64
-
-    vr.volumeData, _ = readTexture3D(dim, dim, dim, "astro64.txt")
     vr.program, _ = createProgram(`
         void main() {
             gl_TexCoord[0] = gl_MultiTexCoord0;
@@ -73,54 +80,119 @@ func CreateVolumetricRenderer() *VolumetricRenderer {
         uniform float angle;
         uniform int width;
         uniform int height;
+        uniform int samples;
 
-        vec3 lerp(vec3 p1, vec3 p2, float t) {
-            return p1 + (p2 - p1) * t;
+        uniform float test;
+
+        float sample(vec3 pos) {
+            float offs = 3.02638 * pow(10.0, 7.0);
+            float mult = offs * 2.0;
+            pos = (pos + vec3(offs)) / mult;
+            if ((pos.x > 0.0) && (pos.x < 1.0) &&
+                (pos.y > 0.0) && (pos.y < 1.0) &&
+                (pos.z > 0.0) && (pos.z < 1.0))
+                {
+                return texture3D(volumeData, pos).r;
+            }
+            return 0.0;
         }
 
-        //vec3 getRay(int i, int j, int nx, int ny) {
-        //    vec3 ru = normalize(-position + up)
-        //    vec3 rv = normalize(-position + ru)
-        //    vec3 rx = ru*(2*tan(angle*PI/360)/nx);
-        //    vec3 ry = rv*(2*tan(angle*PI/360)/ny);
+        vec3 lerp(vec3 p1, vec3 p2, float t) {
+            return p1 + ((p2 - p1) * t);
+        }
 
-        //    return normalize(-position) + 
-        //        (rx * (2*i + 1 - nx) / 2) +
-        //        (ry * (2*j + 1 - ny) / 2);
-        //}
+        vec4 lerp4(vec4 p1, vec4 p2, float t) {
+            return p1 + ((p2 - p1) * t);
+        }
+
+        float delerp(float v1, float v2, float v) {
+            return (v - v1) / (v2 - v1);
+        }
+
+        vec4 transferFunction(float value, float dist) {
+            float t = delerp(0, 15, value);
+            return dist*lerp4(vec4(1, 0, 0, 0), vec4(0, 1, 0, 1), t);
+        }
+
+        vec4 mix(vec4 previousColor, vec4 newColor) {
+            
+            return previousColor + newColor;
+        }
+
+        vec3 getRay(float i, float j, int nx, int ny) {
+            float pi = 3.1415926535897932384626433832795;
+            vec3 ru = normalize(-position * up);
+            vec3 rv = normalize(-position * ru);
+            vec3 rx = ru*(2.0*tan(angle*pi/360.0)/float(width));
+            vec3 ry = rv*(2.0*tan(angle*pi/360.0)/float(height));
+
+            return normalize(-position) + 
+                (rx * ((2.0*i + 1.0 - float(width)) / 2.0)) +
+                (ry * ((2.0*j + 1.0 - float(height)) / 2.0));
+        }
+
+        vec4 sampleRay(vec3 ray) {
+            vec4 color = vec4(0);
+            float len = length(ray) * (far - near);
+            for (int i=0; i<samples; i++) {
+                vec3 point = lerp(ray*near, ray*far, float(i)/float(samples));
+                float value = sample(point);
+                color += transferFunction(value, 0.00000001*len/float(samples));
+            }
+            return color;
+        }
 
         void main(void) {
-            //vec3 start = texture2D(frontPlane, gl_TexCoord[0].st).xyz;
-            //vec3 end   = start * back;
+            vec3 ray = getRay(gl_TexCoord[0].s*float(width),
+                              gl_TexCoord[0].t*float(height),
+                              width, height);
 
-            //float t;
-            //int samples = 100;
-            //gl_FragColor = vec4(0, 0, 0, 255);
-            //for (int i=0; i<samples; i++) {
-            //    t = float(i) / float(samples);
-            //    vec3 pos = lerp(start, end, t);
-            //    float value = texture3D(volumeData, pos).r * float(1)/float(samples);
-            //    value *= 255f;
-            //    gl_FragColor += vec4(value, value, value, 255);
+            gl_FragColor = sampleRay(ray);
+            //gl_FragColor.rgb = ray;
+
+            //gl_FragColor = vec4(0.0);
+            //for (float t = 0.0; t<1.0; t+=0.01) {
+            //    float value = texture3D(volumeData, vec3(t, gl_TexCoord[0].t, gl_TexCoord[0].s)).r;
+
+            //    
+            //    vec4 color = transferFunction(value, 1.0/float(width));
+            //    gl_FragColor = mix(gl_FragColor, color);
             //}
 
-            //gl_FragColor += vec4(0.1, 0, 0, 255);
-            gl_FragColor = vec4(gl_TexCoord[0].s, gl_TexCoord[0].t, 0, 255);
-            //gl_FragColor += vec4(float(width)/100f, 0, 0, 255);
-            //gl_FragColor = vec4(1f, 0, 0, 255);
+            //vec4 color = texture3D(volumeData, vec3(test, gl_TexCoord[0].t, gl_TexCoord[0].s));
+            //gl_FragColor = color * 0.1;
         }
     `)
 
-    //vr.SetSize(400, 400)
+
+    //dim := 512
+    //vr.volumeDataTexture, _ = readTexture3DBinary(dim, dim, dim, "astro512.txt")
+    dim := 64
+    vr.volumeDataTexture, _ = readTexture3D(dim, dim, dim, "astro64.txt")
+
+    vr.volumeData = vr.program.GetUniformLocation("volumeData")
+    vr.volumeData.Uniform1i(0)
 
     vr.position = vr.program.GetUniformLocation("position")
-    vr.position.Uniform3f(0.0, 0.0, 0.0)
+    vr.position.Uniform3f(-8.25e+7, -3.45e+7, 3.35e+7)
 
     vr.up = vr.program.GetUniformLocation("up")
     vr.up.Uniform3f(0.0, 0.0, -1.0)
 
     vr.focus = vr.program.GetUniformLocation("focus")
-    vr.focus.Uniform3f(-8.25e+7, -3.45e+7, 3.35e+7)
+    vr.focus.Uniform3f(0.0, 0.0, 0.0)
+
+    vr.angle = vr.program.GetUniformLocation("angle")
+    vr.angle.Uniform1f(30.0)
+
+    vr.near = vr.program.GetUniformLocation("near")
+    vr.near.Uniform1f(7.5e+7)
+
+    vr.far = vr.program.GetUniformLocation("far")
+    vr.far.Uniform1f(1.4e+8)
+
+    vr.samples = vr.program.GetUniformLocation("samples")
+    vr.samples.Uniform1i(100)
 
     return vr
 }
@@ -133,23 +205,40 @@ func (vr *VolumetricRenderer) SetSize(width int, height int) {
     gl.Viewport(0, 0, width, height)
 }
 
+var t float64 = 0.0;
+
 func (vr *VolumetricRenderer) Draw() {
     gl.Clear(gl.COLOR_BUFFER_BIT)
 
+    gl.MatrixMode(gl.PROJECTION)
+    gl.LoadIdentity()
     gl.MatrixMode(gl.MODELVIEW)
     gl.LoadIdentity()
+
+    t += 0.01;
+    fmt.Printf("Test %v\n", math.Sin(t))
+    vr.test = vr.program.GetUniformLocation("test")
+    vr.test.Uniform1f(float32(math.Sin(t) / 2.0 + 0.5))
+
+    gl.ActiveTexture(gl.TEXTURE0)
+
+    vr.volumeData = vr.program.GetUniformLocation("volumeData")
+    vr.volumeData.Uniform1i(0)
+
+    vr.volumeDataTexture.Bind(gl.TEXTURE_3D)
+    gl.Enable(gl.TEXTURE_3D)
 
     vr.program.Use()
 
     gl.Begin(gl.QUADS)
-    gl.TexCoord3f(0, 0, 0.5)
-    gl.Vertex3f( -1,-1, 0)
-    gl.TexCoord3f(0, 1, 0.5)
-    gl.Vertex3f( -1, 1, 0)
-    gl.TexCoord3f(1, 1, 0.5)
-    gl.Vertex3f(  1, 1, 0)
-    gl.TexCoord3f(1, 0, 0.5)
-    gl.Vertex3f(  1,-1, 0)
+    gl.TexCoord2f(0, 0)
+    gl.Vertex2f( -1,-1)
+    gl.TexCoord2f(0, 1)
+    gl.Vertex2f( -1, 1)
+    gl.TexCoord2f(1, 1)
+    gl.Vertex2f(  1, 1)
+    gl.TexCoord2f(1, 0)
+    gl.Vertex2f(  1,-1)
     gl.End()
 
     vr.window.SwapBuffers()
@@ -157,7 +246,7 @@ func (vr *VolumetricRenderer) Draw() {
 }
 
 func (vr *VolumetricRenderer) Destroy() {
-    vr.volumeData.Delete()
+    vr.volumeDataTexture.Delete()
     vr.window.Destroy()
     glfw.Terminate()
 }
