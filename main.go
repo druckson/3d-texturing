dbf41713a5ae025dac277755510c5da2c89cfc04
Dan Erickson <druckson@gmail.com>
2013-12-21 02:37:10 -0800

    switch to new ScalarField and volumetric render model

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
+//import "fmt"
+
 func main() {
-    vr := CreateVolumetricRenderer()
+    //sf, _ := NewScalarField(100, 100, 100)
+    //sf, _=
+    //val := sf.GetValue(0, 0, 0)
+    //fmt.Printf("Hello %v\n", val)
+
+    sf, _ := readVTKFile("astro64.vtk")
+    vr := CreateVolumetricRenderer(sf, 10.0, 15.0, 9e+7)
+
+    //sf, _ := readVTKFile("proj6B.vtk")
+    //vr := CreateVolumetricRenderer(sf, 0.0, 5.0, 13e+7)
+
     defer vr.Destroy()
 
     for {
